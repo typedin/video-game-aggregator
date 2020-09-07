192c731dfd4c1e48b83238885ebe9ed3ad278a43
AntoineC <antoine@a-f.studio>
2020-09-07 23:45:55 +0200

    can pass kebab case in Image

--- app/Image.php
+++ app/Image.php
@@ -14,17 +14,17 @@ class Image
     const DEFAULT_COVER_URLS = [
         "full" => "/img/default-full-game.jpg",
         "popular" => "/img/default-popular-game.jpg",
-        "comingSoon" => "/img/default-coming-soon-game.jpg",
-        "mostAnticipated" => "/img/default-most-anticipated-game.jpg",
-        "recentlyReviewed" => "/img/default-recently-reviewed-game.jpg",
+        "coming-soon" => "/img/default-coming-soon-game.jpg",
+        "most-anticipated" => "/img/default-most-anticipated-game.jpg",
+        "recently-reviewed" => "/img/default-recently-reviewed-game.jpg",
     ];
 
     const COVER_SIZE = [
         "full" => "cover_big",
         "popular" => "cover_big",
-        "comingSoon" => "cover_small",
-        "recentlyReviewed" => "cover_big",
-        "mostAnticipated" => "cover_small",
+        "coming-soon" => "cover_small",
+        "recently-reviewed" => "cover_big",
+        "most-anticipated" => "cover_small",
     ];
 
     public $url;
@@ -34,19 +34,9 @@ class Image
         $this->url = $url;
     }
 
-    public static function cover(array $params, String $type): Image
+    public static function cover(string $url=null, String $type): Image
     {
-        if (! isset($params["cover"]["url"])) {
-            return new Image(Image::DEFAULT_COVER_URLS[$type]);
-        }
-
-        $url = Image::changeRequestedSizeOfImage(
-            $params["cover"]["url"],
-            Image::COVER_SIZE[$type],
-            "thumb"
-        );
-
-        return new Image($url);
+        return new Image(Image::requestedUrlOrDefault($url, $type));
     }
     
     public function huge()
@@ -62,5 +52,18 @@ class Image
     private static function changeRequestedSizeOfImage($url, $needle, $haystack="thumb")
     {
         return Str::replaceFirst($haystack, $needle, $url);
+    }
+
+    private static function requestedUrlOrDefault($url, $type)
+    {
+        if (! isset($url)) {
+            return Image::DEFAULT_COVER_URLS[$type];
+        }
+
+        return Image::changeRequestedSizeOfImage(
+            $url,
+            Image::COVER_SIZE[$type],
+            "thumb"
+        );
     }
 }
